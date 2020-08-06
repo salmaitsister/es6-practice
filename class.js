f93d1283d2f7ae3c7c958b9d2389f6d2081c3448
Salma Akter <salmaakter@Salmas-MacBook-Pro.local>
2020-08-06 18:34:39 +0600

    knocking at ES6

---
+++ class.js
@@ -0,0 +1,12 @@
+class Student{
+    constructor(sId, sName){
+        this.id = sId;
+        this.name = sName;
+        this.school = "kolimunnesa school"
+    }
+}
+
+const Student1 = new Student(12, "Shuvo");
+const Student2 = new Student(22, "mahiya");
+const Student3 = new Student(29, "Bappi");
+console.log(Student3);
