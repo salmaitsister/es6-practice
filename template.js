8359ccafff7687821127daf6fbb29857448482ea
Salma Akter <salmaakter@Salmas-MacBook-Pro.local>
2020-08-06 15:10:10 +0600

    knocking at ES6

---
+++ template.js
@@ -0,0 +1,15 @@
+const firstName = "Justin "
+const LastName ="TimberLake"
+const fullName = firstName + " "+ LastName + " is a good boy";
+const fullName2 = '${firstName} ${20+50+30} is a good boy.';
+
+const multiline = "I Love you\n"
++"I miss you\n"
++"I need you";
+
+const multiline2 = `I love you
+I miss you
+Node. I don't need you
+Baily road e dorkar nai.`
+
+console.log(multiline2);
